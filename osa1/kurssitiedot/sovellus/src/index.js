54381359eee4d6a3800871bf4352e8b9eaed2c7c
danieladasilva <dasilva.daniela@outlook.com>
2019-08-12 15:17:03 +0300

    kokeillaan, onko Fullstack2019 repositorio

---
+++ osa1/kurssitiedot/sovellus/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = () => {
+  return (
+    <div>
+      <p> Header </p>
+    </div>  
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
+      <p> Content </p>
+    </div>  
+  )
+}
+
+const Total = () => {
+  return (
+    <div>
+      <p> Total </p>
+    </div>  
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
